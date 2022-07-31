26a36d49f07c9b8903cb641bfa75c948f1982fef
RPE <vitali.shuman96@gmail.com>
2022-07-31 16:28:41 +0300

    20/25

--- script.js
+++ script.js
@@ -2,3 +2,39 @@ const gallery = document.querySelector(".gallery");
 const overlay = document.querySelector(".overlay");
 const image = document.querySelector("img");
 const button = document.querySelector(".close");
+
+function generateElement([width, height]) {
+  return `
+		<div class="item w${width} h${height}">
+			<img src="./images/${random(12)}.jpg">
+			<div class="item__overlay">
+				<button>View</button>
+			</div>
+		</div>
+	`;
+}
+
+function random(max) {
+  return Math.floor(Math.random() * max) + 1;
+}
+
+function createGridArray(length, w, w) {
+  return Array.from({ length: length }, () => [random(w), random(w)]);
+}
+
+const digits = [...createGridArray(50, 4, 4), ...createGridArray(20, 1, 1)];
+
+const newHTML = digits.map(generateElement).join("");
+gallery.innerHTML = newHTML;
+
+document.querySelectorAll(".item").forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const src = event.currentTarget.querySelector("img").src;
+    image.src = src;
+    overlay.classList.add("open");
+  });
+});
+
+button.addEventListener("click", () => {
+  overlay.classList.remove("open");
+});
